604896472e02ea4317f689e1dfb4e1957c7572cd
ElayBPS <wasavmada@gmail.com>
2019-04-17 16:33:24 +0100

    it worked

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 8888
 
 server.use(express.static(__dirname + '/website/'))
 
+server.get("/test", (req,res) => {
+    res.send("It worked")
+})
+
 server.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
 })
